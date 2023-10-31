8c8092316341920e65d892839f2b2ef7517d3baf
Sajana Sulakshana <sajanaroxx@gmail.com>
2023-10-31 10:26:21 +0530

    Initial commit

---
+++ Game/Ludo/NavigationData/NavModel.js
@@ -0,0 +1,213 @@
+const E1 = new GetNavObj($('#E1'));
+const E2 = new GetNavObj($('#E2'));
+const E3 = new GetNavObj($('#E3'));
+const E4 = new GetNavObj($('#E4'));
+const E5 = new GetNavObj($('#E5'));
+const E6 = new GetNavObj($('#E6'));
+const E7 = new GetNavObj($('#E7'));
+const E8 = new GetNavObj($('#E8'));
+const E9 = new GetNavObj($('#E9'));
+const E10 = new GetNavObj($('#E10'));
+
+const E11 = new GetNavObj($('#E11'));
+const E12 = new GetNavObj($('#E12'));
+const E13 = new GetNavObj($('#E13'));
+const E14 = new GetNavObj($('#E14'));
+const E15 = new GetNavObj($('#E15'));
+const E16 = new GetNavObj($('#E16'));
+const E17 = new GetNavObj($('#E17'));
+const E18 = new GetNavObj($('#E18'));
+const E19 = new GetNavObj($('#E19'));
+const E20 = new GetNavObj($('#E20'));
+
+const E21 = new GetNavObj($('#E21'));
+const E22 = new GetNavObj($('#E22'));
+const E23 = new GetNavObj($('#E23'));
+const E24 = new GetNavObj($('#E24'));
+const E25 = new GetNavObj($('#E25'));
+const E26 = new GetNavObj($('#E26'));
+const E27 = new GetNavObj($('#E27'));
+const E28 = new GetNavObj($('#E28'));
+const E29 = new GetNavObj($('#E29'));
+const E30 = new GetNavObj($('#E30'));
+
+const E31 = new GetNavObj($('#E31'));
+const E32 = new GetNavObj($('#E32'));
+const E33 = new GetNavObj($('#E33'));
+const E34 = new GetNavObj($('#E34'));
+const E35 = new GetNavObj($('#E35'));
+const E36 = new GetNavObj($('#E36'));
+const E37 = new GetNavObj($('#E37'));
+const E38 = new GetNavObj($('#E38'));
+const E39 = new GetNavObj($('#E39'));
+const E40 = new GetNavObj($('#E40'));
+
+const E41 = new GetNavObj($('#E41'));
+const E42 = new GetNavObj($('#E42'));
+const E43 = new GetNavObj($('#E43'));
+const E44 = new GetNavObj($('#E44'));
+const E45 = new GetNavObj($('#E45'));
+const E46 = new GetNavObj($('#E46'));
+const E47 = new GetNavObj($('#E47'));
+const E48 = new GetNavObj($('#E48'));
+const E49 = new GetNavObj($('#E49'));
+const E50 = new GetNavObj($('#E50'));
+
+const E51 = new GetNavObj($('#E51'));
+const E52 = new GetNavObj($('#E52'));
+const E53 = new GetNavObj($('#E53'));
+const E54 = new GetNavObj($('#E54'));
+const E55 = new GetNavObj($('#E55'));
+const E56 = new GetNavObj($('#E56'));
+const E57 = new GetNavObj($('#E57'));
+const E58 = new GetNavObj($('#E58'));
+const E59 = new GetNavObj($('#E59'));
+const E60 = new GetNavObj($('#E60'));
+
+const E61 = new GetNavObj($('#E61'));
+const E62 = new GetNavObj($('#E62'));
+const E63 = new GetNavObj($('#E63'));
+const E64 = new GetNavObj($('#E64'));
+const E65 = new GetNavObj($('#E65'));
+const E66 = new GetNavObj($('#E66'));
+const E67 = new GetNavObj($('#E67'));
+const E68 = new GetNavObj($('#E68'));
+const E69 = new GetNavObj($('#E69'));
+const E70 = new GetNavObj($('#E70'));
+const E71 = new GetNavObj($('#E71'));
+const E72 = new GetNavObj($('#E72'));
+
+const FINAL_GREEN= new GetNavObj($('#finalGreen'));
+const FINAL_YELLOW= new GetNavObj($('#finalYellow'));
+const FINAL_RED= new GetNavObj($('#finalRed'));
+const FINAL_BLUE= new GetNavObj($('#finalBlue'));
+
+function GetNavObj(obj) {
+
+    let block = obj;
+
+
+    this.getBlock = function () {
+        return block;
+    }
+    this.addCoin = function (color) {
+        switch (color) {
+            case "red":
+                appendCoin("assets/images/redCoin.png", 'red');
+                break;
+            case "green":
+                appendCoin("assets/images/greenCoin.png", 'green');
+                break;
+            case "yellow":
+                appendCoin("assets/images/yellowCoin.png", 'yellow');
+                break;
+            case "blue":
+                appendCoin("assets/images/blueCoin.png", 'blue');
+                break;
+        }
+    }
+
+    function reArrangeCoins() {
+        let children = block.children();
+        switch (children.length) {
+
+            case 2: {
+                children.eq(0).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'left': '0'
+                });
+                children.eq(1).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'right': '0'
+                });
+                break;
+            }
+
+            case 3: {
+                children.eq(0).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'top': '-20%',
+                    'left': '0'
+                });
+                children.eq(1).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'top': '-20%',
+                    'right': '0'
+                });
+                children.eq(2).css({
+                    'inset': '0',
+                    'scale': '.8'
+                });
+                break;
+            }
+            case 4: {
+                children.eq(0).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'top': '-20%',
+                    'left': '0'
+                });
+                children.eq(1).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'top': '-20%',
+                    'right': '0'
+                });
+                children.eq(2).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'bottom': '-20%',
+                    'left': '0'
+                });
+                children.eq(3).css({
+                    'inset': 'initial',
+                    'scale': '.8',
+                    'bottom': '-20%',
+                    'right': '0'
+                });
+                break;
+            }
+        }
+    }
+
+    function appendCoin(img, cls) {
+        block.append('<img class="' + cls + '" style="transform: scale(0)" src="' + img + '">');
+        block.children().css('transform', 'scale(1)');
+        reArrangeCoins();
+        sendBack(cls);
+    }
+
+    function sendBack(cls){
+        let count=0;
+        block.children().each(function () {
+            if(count==0) {
+                let tempCoin = $(this);
+                if (tempCoin.attr('class') != cls) {
+                    tempCoin.remove();
+                    turnCount=0;
+                    retrieveCoin(tempCoin.attr('class'));
+                }
+                count++;
+            }
+        });
+    }
+
+    let count = 0;
+    this.removeCoin = function (color) {
+        block.children("img").each(function () {
+            if (count == 0) {
+                if ($(this).attr('class') == color) {
+                    $(this).remove();
+                    reArrangeCoins();
+                    count++;
+                }
+            }
+        });
+        count = 0;
+    }
+}
+
---
+++ Game/Ludo/model/user.js
@@ -0,0 +1,198 @@
+let users = [
+    new User("user1", NavPath_1, "red", $('#cr1'), $('#cr2'), $('#cr3'), $('#cr4')),
+    new User("user2", NavPath_2, "green", $('#cg1'), $('#cg2'), $('#cg3'), $('#cg4')),
+    new User("user3", NavPath_3, "yellow", $('#cy1'), $('#cy2'), $('#cy3'), $('#cy4')),
+    new User("user4", NavPath_4, "blue", $('#cb1'), $('#cb2'), $('#cb3'), $('#cb4'))];
+
+function User(name, navPath, coinColor, place1, place2, place3, place4) {
+    this.name = name;
+    let score = 0;
+    let path = navPath;
+    let color = coinColor;
+    let cPlacement = [place1, place2, place3, place4];
+
+
+    this.getScore = function () {
+        return score;
+    }
+
+    this.setScore = function (scr) {
+        score = scr;
+    }
+
+    this.getPath = function () {
+        return path;
+    }
+
+    this.getCoinCount = function () {
+        return cPlacement.length;
+    }
+
+    function unbindPathBlocks() {
+        path.forEach((item, index) => {
+            item.getBlock().children().each(function () {
+                if ($(this).attr('class') == color && ((path.length - (index + 1)) >= random)) {
+                    item.getBlock().unbind();
+                    item.getBlock().css('animation', 'none');
+                }
+            });
+        });
+    }
+
+
+    this.animateCoinsOnPath = function () {
+        let count = 0;
+        path.forEach((item, index) => {
+
+            item.getBlock().children().each(function () {
+                if (count == 0) {
+
+                    if ($(this).attr('class') == color && (path.length - (index + 1)) >= random) {
+                        item.getBlock().css('animation', 'none');
+                        item.getBlock().css('animation', '.5s animateBorder infinite');
+
+                        item.getBlock().click(function () {
+                            unbindPathBlocks();
+                            endCoinAnimation();
+                            let navPath = path.slice(index, (index + random + 1));
+                            let i = 0;
+                            let inter = setInterval(function () {
+                                if (i != navPath.length - 1) {
+                                    let jump = new Audio("assets/audio/jump.wav")
+                                    jump.play();
+                                    navPath[i].removeCoin(color);
+                                    navPath[i + 1].addCoin(color);
+
+                                    if (path.indexOf(navPath[i + 1]) == (path.length - 1)) {
+                                        turnCount = 0;
+                                        checkWinner();
+                                        turn = true;
+                                    }
+
+                                    i++;
+                                } else {
+                                    if ((random != 6) && (turnCount != 0)) {
+                                        switchUser();
+                                        turn = true;
+                                    }
+                                    clearInterval(inter);
+                                    turn = true;
+                                }
+                            }, 250);
+
+                        });
+                        count++;
+                    }
+                }
+            });
+
+            count = 0;
+        });
+    }
+
+    this.animateAvailableCoins = function () {
+        cPlacement.forEach((item, index) => {
+            let temp = document.getElementById($(item).attr('id'));
+            temp.style.animation = 'none';
+            setTimeout(function () {
+                temp.style.animation = '.5s animateBorder infinite';
+            }, 1);
+
+            item.click(function () {
+                endCoinAnimation();
+                unbindPathBlocks();
+                item.children().css('transform', 'scale(0)');
+                item.children().remove();
+                cPlacement.splice(cPlacement.indexOf(item), 1);
+                path[0].addCoin(color);
+                turn = true;
+            });
+        });
+    }
+
+    let endCoinAnimation = function () {
+        cPlacement.forEach((item, index) => {
+            let temp = document.getElementById($(item).attr('id'));
+            temp.style.animation = 'none';
+            item.unbind();
+        });
+    }
+    this.endAnimation = endCoinAnimation();
+}
+
+
+function GetCurrentUser() {
+    let userItr = [users[0], users[1], users[2], users[3]];
+
+    this.getUser = function () {
+        return userItr[0];
+    }
+
+    this.switchUser = function () {
+        let tempUser = userItr.shift();
+        userItr.push(tempUser);
+    }
+}
+
+function retrieveCoin(cls) {
+    switch (cls) {
+        case 'red': {
+            let count = 0;
+            $('.redCoinSurface').children().children().each(function () {
+                if (count == 0) {
+                    let tempPlacement = $(this);
+                    console.log(tempPlacement);
+                    if ((tempPlacement.children().length) == 0) {
+                        tempPlacement.append(`<img class="${cls}" src="../assets/images/redCoin.png">`);
+                        count++;
+                    }
+
+                }
+            });
+            break;
+        }
+
+        case 'green': {
+            let count = 0;
+            $('.greenCoinSurface').children().children().each(function () {
+                if (count == 0) {
+                    let tempPlacement = $(this);
+                    if ((tempPlacement.children().length) == 0) {
+                        tempPlacement.append(`<img class="${cls}" src="../assets/images/greenCoin.png">`);
+                        count++;
+                    }
+
+                }
+            });
+            break;
+        }
+        case 'yellow': {
+            let count = 0;
+            $('.yellowCoinSurface').children().children().each(function () {
+                if (count == 0) {
+                    let tempPlacement = $(this);
+                    if ((tempPlacement.children().length) == 0) {
+                        tempPlacement.append(`<img class="${cls}" src="../assets/images/yellowCoin.png">`);
+                        count++;
+                    }
+
+                }
+            });
+            break;
+        }
+        case 'blue': {
+            let count = 0;
+            $('.blueCoinSurface').children().children().each(function () {
+                if (count == 0) {
+                    let tempPlacement = $(this);
+                    if ((tempPlacement.children().length) == 0) {
+                        tempPlacement.append('<img class="' + cls + '" style="transform: scale(0)" src="../assets/images/blueCoin.png">');
+                        count++;
+                    }
+
+                }
+            });
+            break;
+        }
+    }
+}
